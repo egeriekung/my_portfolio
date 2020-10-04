ae3a8c40b8bfda8f12c14dd0785c0020ea9492e0
Egerie <egeriekung.preface@gmail.com>
2020-10-04 12:04:17 +0000

    remove old stuff

--- personal_port/settings.py
+++ personal_port/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^kay%au91yriasg_c6sr)f3zr*w0v*1b4ub@otm0y^)ibg*#by'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['egerielovespink.pythonanywhere.com']
 
 
 # Application definition
@@ -122,5 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
